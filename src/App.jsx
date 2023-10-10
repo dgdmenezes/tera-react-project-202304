e7c78df10a7bfecc642ba7e89fbd411cf93eb55b
Dante Menezes <dgdmenezes@gmail.com>
2023-10-10 19:32:15 -0300

    Adicionando roteamento

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-//import Home from "./components/pages/Home";
-//import Users from "./components/pages/Users";
-//import Blog from "./components/pages/UserBlog";
-import CreatPost from "./components/pages/CreatePost";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Users from "./components/pages/Users";
+import UserBlog from "./components/pages/UserBlog";
+import PostForm from "./components/pages/PostForm";
 
 import "./styles/normalize.css";
 import "./styles/fontawesome.min.css";
 import "./styles/main.css";
 
 function App() {
-  return <CreatPost />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/:userId" element={<UserBlog />} />
+        <Route path="/users/:userId/post" element={<PostForm />} />
+        <Route
+          path="*"
+          element={
+            <img
+              src="https://httpstatusdogs.com/img/404.jpg"
+              alt="404 Not Found"
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
